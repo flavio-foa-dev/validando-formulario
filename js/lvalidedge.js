be371fa79fea067bb076bc6e764553210194fc91
Flavio Andrade <foa12381@gmail.com>
2023-03-07 13:26:25 -0300

    end

--- js/lvalidedge.js
+++ js/lvalidedge.js
@@ -1,14 +1,14 @@
 export function legalAge(age) {
-  const dateNasc = new Date(age)
+  const dateNasc = new Date(age.value)
 
   const dateAtual = new Date()
   const dateNascMaisDezoit0 = new Date(dateNasc.getUTCFullYear() + 18, dateNasc.getUTCMonth(), dateNasc.getUTCDate())
   const result = dateAtual >= dateNascMaisDezoit0
 
   console.log(result, ">>>>>>>")
-
-
-
+  if (!result) {
+    age.setCustomValidity('O user do not legal age"')
+  }
 
 }
 
---
+++ js/photo.js
@@ -0,0 +1,44 @@
+const btnPhoto = document.querySelector('[data-video-botao]')
+const fieldCamera = document.querySelector('[data-camera]')
+const movie = document.querySelector('[data-video]')
+const btnTirarPhoto = document.querySelector('[data-tirar-foto]')
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
+
+let imagemURL = "";
+
+btnPhoto.addEventListener("click", async() => {
+  const camera = await navigator.mediaDevices
+    .getUserMedia({video: true, audio: false})
+
+    btnPhoto.style.display = "none"
+    fieldCamera.style.display = "block"
+
+    movie.srcObject = camera
+})
+
+btnTirarPhoto.addEventListener("click", () => {
+ console.log("Tirar Photo")
+
+  canvas.getContext('2d').drawImage(movie, 0, 0, canvas.width, canvas.height);
+
+  imagemURL = canvas.toDataURL('image/jpeg');
+
+  fieldCamera.style.display = "none"
+  mensagem.style.display = "block"
+
+  //movie.srcObject.getTracks().forEach(track => track.stop())
+
+})
+
+
+botaoEnviarFoto.addEventListener("click", () =>{
+  const dadoCad = JSON.parse(localStorage.getItem('userCad'))
+  dadoCad.image = imagemURL;
+
+  console.log(dadoCad)
+  localStorage.setItem('userCad', JSON.stringify(dadoCad))
+
+  window.location.href = "../pages/abrir-conta-form-3.html"
+})
